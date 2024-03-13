a6ef68b08105ff25f91fee8f1ae7733c858323fb
emmonbear <moskaleviluak@icloud.com>
2024-03-13 16:05:09 +0500

    feat: add main header

---
+++ src/smartcalc.h
@@ -0,0 +1,14 @@
+/**
+ * @file smartcalc.h
+ * @author Moskalev Ilua (https://github.com/emmonbear)
+ * @brief Main header
+ * @version 1.0
+ * @date 2024-02-22
+ *
+ * Copyright (c) emmonbear. All rights reserved.
+ *
+ */
+#ifndef SMARTCALC_H
+#define SMARTCALC_H
+
+#endif  // SMARTCALC_H
